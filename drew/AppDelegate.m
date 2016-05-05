469c28d368cf09b8f8dd1d9f6275c4354d52b592
Fucker <290358009@qq.com>
2016-05-05 16:11:31 +0800

    fist

--- drew/AppDelegate.m
+++ drew/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -16,6 +16,10 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    [self.window makeKeyAndVisible];
+    self.window.rootViewController = [ViewController new];
+    
     // Override point for customization after application launch.
     return YES;
 }
